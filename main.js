567432dcc19ce7adc9f955aaf1ca66ddfa7ed778
Shakir <shakir.z.aziz@gmail.com>
2020-05-29 00:20:36 -0400

    kata solved

---
+++ main.js
@@ -0,0 +1,65 @@
+  
+// kata01 add
+
+  function add(value1, value2){
+    return value1 + value2
+}
+console.log ('Add', add(2, 4))
+
+// // // Kata02 Multiply
+
+function multiply(value1, value2){
+    let answer = value1
+    for(var i = 0; i < value2 - 1; i++){
+      answer += value1
+    }
+    return answer;
+  }
+  console.log('Multiplication', multiply(4, 6))
+
+
+
+
+//   khata03 Power/Exponentiation
+
+function power(value1, value2){
+    let answer =01;
+   
+    for(var i1 = 0; i1 < value2; i1 +=1){
+        big = multiply(value1,1);
+        answer = multiply(big,answer)
+    }
+    
+    return answer;
+  }
+  console.log('Power', power(3, 4))
+
+
+// // kata04
+function factorial(fc){
+    let answer =1;
+    let result = fc
+   
+    for(var i2 =0;i2<fc;i2 +=1){
+        big = multiply(result,1);
+        answer = multiply(big,answer)
+        result=add(result,-1)
+    }
+    
+    return answer;
+  }
+  
+  console.log('Factorial', factorial(6))
+
+//   Extra kata Fibonacci
+
+function fib (f) {
+let value1 = 0; value2 = 1; value3 = 1;
+for (var i3 = 2; i3 <= f; i3++) {
+value3 = value1 + value2;
+value1 = value2;
+value2 = value3;
+}
+return value3;
+}
+console.log('Fibonacci', fib(8))
