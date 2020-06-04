bb521e1e87c1a3377249b63ccba70ddef27e7758
Shakir <shakir.z.aziz@gmail.com>
2020-06-03 20:26:47 -0400

    math operators removed

--- main.js
+++ main.js
@@ -10,22 +10,19 @@ console.log ('Add', add(2, 4))
 
 function multiply(value1, value2){
     let answer = value1
-    for(var i = 0; i < value2 - 1; i++){
-      answer += value1
+    for(var i = 0; i < value2 - 1; i= add(i, 1)){
+      answer = add(answer, value1)
     }
     return answer;
   }
   console.log('Multiplication', multiply(4, 6))
-
-
-
 
 //   khata03 Power/Exponentiation
 
 function power(value1, value2){
     let answer =01;
    
-    for(var i1 = 0; i1 < value2; i1 +=1){
+    for(var i1 = 0; i1 < value2; i1= add(i1, 1)){
         big = multiply(value1,1);
         answer = multiply(big,answer)
     }
@@ -40,7 +37,7 @@ function factorial(fc){
     let answer =1;
     let result = fc
    
-    for(var i2 =0;i2<fc;i2 +=1){
+    for(var i2 =0;i2<fc;i2 =add(i2,1)){
         big = multiply(result,1);
         answer = multiply(big,answer)
         result=add(result,-1)
@@ -55,11 +52,12 @@ function factorial(fc){
 
 function fib (f) {
 let value1 = 0; value2 = 1; value3 = 1;
-for (var i3 = 2; i3 <= f; i3++) {
-value3 = value1 + value2;
+for (var i3 = 2; i3 <= f; i3= add(i3, 1)) {
+value3 = value1  = add(value1, value2);
 value1 = value2;
 value2 = value3;
 }
 return value3;
 }
 console.log('Fibonacci', fib(8))
+
